051bc3d68c468f8125f907748e155ad76fa61cc4
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2024-04-07 14:43:03 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const closeButtonContainer = document.querySelector(
     ".close-sidebar-btn-container"
   );
+  const popupDiv = document.querySelector(".close-popup");
+
+  const newConversationBtn = document.querySelector(
+    ".new-conversation-btn-link-pencile-icon"
+  );
+  const newChatPopup = document.querySelector(".new-chat-popup");
+  const dallNewChat = document.querySelector(".dall-e-new-icon");
+  const logoCreatorNewChat = document.querySelector(".logo-creator-new-icon");
 
   function updateButtonState() {
     if (sidebar.classList.contains("open")) {
@@ -30,11 +38,40 @@ document.addEventListener("DOMContentLoaded", function () {
       document.documentElement.style.setProperty("--sidebar-width", "260px");
       closeButtonContainer.style.transform =
         "translateX(260px) translateY(-50%) rotate(0deg) translateZ(0px)";
+      popupDiv.textContent = "Close sidebar";
     } else {
       document.documentElement.style.setProperty("--sidebar-width", "0px");
       closeButtonContainer.style.transform =
         "translateY(-50%) rotate(0deg) translateZ(0px)";
+      popupDiv.textContent = "Open sidebar";
     }
+  });
+
+  newConversationBtn.addEventListener("mouseenter", function () {
+    newChatPopup.style.top = "0px";
+    newChatPopup.style.display = "inline";
+  });
+
+  newConversationBtn.addEventListener("mouseleave", function () {
+    newChatPopup.style.display = "none";
+  });
+
+  dallNewChat.addEventListener("mouseenter", function () {
+    newChatPopup.style.top = "40px";
+    newChatPopup.style.display = "inline";
+  });
+
+  dallNewChat.addEventListener("mouseleave", function () {
+    newChatPopup.style.display = "none";
+  });
+
+  logoCreatorNewChat.addEventListener("mouseenter", function () {
+    newChatPopup.style.top = "85px";
+    newChatPopup.style.display = "inline";
+  });
+
+  logoCreatorNewChat.addEventListener("mouseleave", function () {
+    newChatPopup.style.display = "none";
   });
 
   updateButtonState();
